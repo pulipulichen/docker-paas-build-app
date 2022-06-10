6867dc1bc880355131d03d00bdaaeebfd1ac5b47
Pulipuli Chen <pulipuli.chen@gmail.com>
2022-06-10 18:01:33 +0800

    還在解決only_update_app的問題。Fri Jun 10 06:01:33 PM CST 2022

--- scripts/BuildDockerfile.js
+++ scripts/BuildDockerfile.js
@@ -140,8 +140,8 @@ RUN ls -l ${path.join(containerAppFolder,REPO_NAME)}
 
 # APP
 RUN rm -rf ${path.join(app_path_parent,app_path_basename)} || echo "No folder: ${path.join(app_path_parent,app_path_basename)}"
-RUN ln -s ${path.join(containerAppFolder, REPO_NAME)} ${path.join(app_path_parent)}
-RUN mv ${path.join(app_path_parent, REPO_NAME)} ${path.join(app_path_parent, app_path_basename)} || echo "Same folder name: ${path.join(app_path_parent,app_path_basename)}"
+RUN ln -s ${path.join(containerAppFolder, REPO_NAME)} /tmp
+RUN mv ${path.join('/tmp', REPO_NAME)} ${path.join(app_path_parent, app_path_basename)} || echo "Same folder name: ${path.join(app_path_parent,app_path_basename)}"
 
 RUN ls -l ${path.join(app_path_parent)}
 RUN ls -l ${path.join(containerAppFolder, REPO_NAME)}
