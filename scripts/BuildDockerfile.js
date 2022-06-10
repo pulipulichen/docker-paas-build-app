2934b3833f9406ab2933f9eaf636984087b9d9b4
Pulipuli Chen <pulipuli.chen@gmail.com>
2022-06-10 17:49:56 +0800

    還在解決only_update_app的問題。Fri Jun 10 05:49:56 PM CST 2022

--- scripts/BuildDockerfile.js
+++ scripts/BuildDockerfile.js
@@ -136,11 +136,16 @@ RUN git config --global user.name "${username}"
 RUN git checkout -b ${REPO} || git checkout ${REPO}
 RUN git config --global pull.rebase true
 
+RUN ls -l ${path.join(containerAppFolder,REPO_NAME)}
+
 # APP
 #RUN rm -rf ${path.join(app_path_parent,app_path_basename)} || echo "No folder: ${path.join(app_path_parent,app_path_basename)}"
 #RUN rm -rf ${path.join(app_path_parent,app_path_basename)} || echo "No folder: ${path.join(app_path_parent,app_path_basename)}"
-RUN ln -s ${path.join(containerAppFolder, REPO_NAME)} ${path.join(app_path_parent, app_path_basename)}
+RUN ln -s ${path.join(containerAppFolder, REPO_NAME)} ${path.join(app_path_parent)}
 RUN mv ${path.join(app_path_parent, REPO_NAME)} ${path.join(app_path_parent, app_path_basename)} || echo "Same folder name: ${path.join(app_path_parent,app_path_basename)}"
+
+RUN ls -l ${path.join(app_path_parent)}
+RUN ls -l ${path.join(containerAppFolder, REPO_NAME)}
 `
 
   let dockerfileCopy = `
